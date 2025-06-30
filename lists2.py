d924365f8c2841b113e07291ac15a66a619d15ce
michaeldlamini <michaelmini67@gmail.com>
2025-06-30 12:05:51 +0200

    Create lists2.py

---
+++ lists2.py
@@ -0,0 +1,19 @@
+fruits = ["grapes", "kiwi", "apple"]
+
+print(fruits)
+
+fruits.append("mango")
+
+print(fruits)
+
+fruits.insert(2, "pear")
+
+print(fruits)
+
+fruits.remove("apple")
+
+print(fruits)
+
+fruits.sort(reverse=True)
+
+print(fruits)
