ecbc28cffe57b5afdce5cc6a6b5905f74a62180d
michaeldlamini <michaelmini67@gmail.com>
2025-06-30 12:36:03 +0200

    Create tuples.py

---
+++ tuples.py
@@ -0,0 +1,17 @@
+#Tuples
+
+my_tuples = (1, 2, 3, 4, 5)
+
+print(my_tuples)
+
+print(my_tuples[0])
+print(my_tuples[1])
+print(my_tuples[2])
+print(my_tuples[-1])
+
+tuples1 = (2, 4, 6)
+tuples2 = (8, 10, 12)
+
+conc_tuples = tuples1 + tuples2
+rep_tuples = 3 * tuples2
+print(rep_tuples)
