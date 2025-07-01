ab298051197f9672974c17bcef395cc8413e188c
michaeldlamini <michaelmini67@gmail.com>
2025-07-01 13:26:22 +0200

    Update shopping_cart.py

--- shopping_cart.py
+++ shopping_cart.py
@@ -3,3 +3,26 @@
 # Have an exit clause if the user wishes to stop adding more things to their cart
 # Show food items and how much it costs at the end
 
+foods = []
+prices = []
+total = 0
+
+while True:
+    food = input("Enter a food to buy or press q to quit: ")
+    if food.lower() == 'q':
+        break
+    else:
+        price = float(input(f"Enter the price to the {food}: R"))
+        foods.append(food)
+        prices.append(price)
+        
+        print("-----YOUR SHOPPING CART-----")
+        
+        for food in foods:
+            print(food, end= " ")
+            
+        for price in prices:
+            total += price
+            
+            print("\n")
+            print(f"Your total is: R{total}")
