cbc6aed481321121287ed86892de5f9cb2b94276
michaeldlamini <michaelmini67@gmail.com>
2025-07-01 04:49:25 +0200

    Create dictionaries2.py

---
+++ dictionaries2.py
@@ -0,0 +1,22 @@
+#Operators
+
+#Addition (+)
+#Subtraction (-)
+#Multiplication (*)
+#Division (/)
+#Modulus (%)
+#Exponents (**)
+
+x = 6
+y = 2
+v = 9
+
+v *= 2
+print(v)
+
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x%y)
+print(x**y)
